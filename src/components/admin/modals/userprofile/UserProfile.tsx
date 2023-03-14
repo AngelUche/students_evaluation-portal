00939091b7fcf579f6c0241c669dbe1eb284cae5
mightGuy <onyejemeemmanuel65@gmail.com>
2023-03-14 05:30:33 +0100

    User Profile UI updates | Admin section

--- src/components/admin/modals/userprofile/UserProfile.tsx
+++ src/components/admin/modals/userprofile/UserProfile.tsx
@@ -13,7 +13,7 @@ function UserProfileModal() {
     const [selectedMember, setSlectedMember] = useState("");
 
     return (
-        <div className="w-[350px] py-8 px-8 bg-white rounded-[10px] overflow-hidden shadow-xl flex flex-col gap-y-5" onClick={(e) => {
+        <div className="w-[350px] py-8 px-8 bg-white rounded-[10px] overflow-hidden shadow-xl flex flex-col gap-y-6" onClick={(e) => {
             e.stopPropagation()
         }}>
             <div className="flex text-gray-700 items-center gap-3">
@@ -23,8 +23,8 @@ function UserProfileModal() {
             <div className="flex flex-col gap-y-5">
                 <div>
                     <select
-                        className="w-full p-3"
-                        aria-label="Select a breed of dog to display results"
+                        className="w-full p-3 rounded outline-none focus:border-2 focus:border-[#0f5257]"
+                        aria-label="Select a list of class names to get available students"
                         value={selectedClass}
                         onChange={(event) => {
                             setSelectedClass(event.target.value)
@@ -49,8 +49,8 @@ function UserProfileModal() {
                 <div>
                     <div>
                         <select
-                            className="w-full p-3"
-                            aria-label="Select a breed of dog to display results"
+                            className="w-full p-3 rounded outline-none focus:border-2 focus:border-[#0f5257]"
+                            aria-label="Select a list of members of a class to view profile"
                             value={selectedMember}
                             onChange={(event) => setSlectedMember(event.target.value)}
                         >
@@ -74,7 +74,7 @@ function UserProfileModal() {
                     </div>
                 </div>
             </div>
-            <button className="w-full uppercase py-2 bg-[blue] text-white ">View full profile</button>
+            <button className="w-full rounded uppercase py-2 bg-[blue] hover:bg-[#0202c5] hover:shadow-xl text-white ">View full profile</button>
         </div>
     );
 }
